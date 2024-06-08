16b027e481714fca544cf191f5217e7c1c503988
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2024-06-08 15:47:20 +0000

    fix Union error

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -4,6 +4,7 @@
 from typing import List, Union
 
 
-def sum_mixed_list(mxd_list: List[Union[int | float]]) -> float:
+def sum_mixed_list(mxd_list: List[Union[int, float]]) -> float:
     """returns the sum of a list"""
     return sum(mxd_list)
+
