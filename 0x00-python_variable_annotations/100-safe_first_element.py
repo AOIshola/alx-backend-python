48c28786a3897fd162ceb6ccedc196d25ecbe846
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2024-06-08 16:07:38 +0000

    safe first element

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Duck-Typed annotations"""
+
+from typing import Any, Sequence, Union, TypeVar
+
+
+T = TypeVar('T')
+NoneType = TypeVar('NoneType', bound=None)
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, NoneType]:
+    """return first element"""
+    if lst:
+        return lst[0]
+    else:
+        return None
