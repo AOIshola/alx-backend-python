60a37dd1a27167e12563c34b3486029a21c09435
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2024-06-11 05:52:19 +0000

    use python async function

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -8,6 +8,6 @@ from typing import TypeVar
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay) -> asyncio.Task:
+def task_wait_random(max_delay: int) -> asyncio.Task:
     """Returns asyncio task type"""
     return asyncio.create_task(wait_random(max_delay))
